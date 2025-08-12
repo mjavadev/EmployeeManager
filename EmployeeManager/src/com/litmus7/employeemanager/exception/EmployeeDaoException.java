c9ecc8b7a41f7869e2a7b6af08fc1d3ff97b4660
Mohammed Javad A A <mohammedjavad271@gmail.com>
2025-08-12 20:03:53 +0530

    updated and added more specific layer-wise custom exceptions

---
+++ EmployeeManager/src/com/litmus7/employeemanager/exception/EmployeeDaoException.java
@@ -0,0 +1,8 @@
+package com.litmus7.employeemanager.exception;
+
+public class EmployeeDaoException extends Exception {
+	
+	public EmployeeDaoException (String message, Throwable cause) {
+		super(message, cause);
+	}
+}
---
+++ EmployeeManager/src/com/litmus7/employeemanager/exception/EmployeeNotFoundException.java
@@ -0,0 +1,9 @@
+package com.litmus7.employeemanager.exception;
+
+public class EmployeeNotFoundException extends Exception {
+
+	public EmployeeNotFoundException(String message) {
+		super(message);
+	}
+	
+}
